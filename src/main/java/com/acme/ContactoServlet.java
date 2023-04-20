7765d734157738a32ef8b47b2e0c7f649d811a34
juan <juan.orih@gmail.com>
2023-04-19 18:42:51 -0600

    Contacto Get

---
+++ src/main/java/com/acme/ContactoServlet.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.acme;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author JuaN
+ */
+@WebServlet(urlPatterns = "/ContactoServlet")
+public class ContactoServlet extends HttpServlet{
+    
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+        String nombre = req.getParameter("nombre");
+        String emailId = req.getParameter("emailId");
+        String telefono = req.getParameter("telefono");
+        String descripcion = req.getParameter("descripcion");
+        
+        resp.setContentType("text/html");
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.print("<html>");
+        printWriter.print("<body>");
+        printWriter.print("<h1> Datos de Registro de Contacto </h1>");
+        printWriter.print("<p>Nombre: " +nombre +"</p>");
+        printWriter.print("<p>Email: " +emailId +"</p>");
+        printWriter.print("<p>Telefono: " +telefono +"</p>");
+        printWriter.print("<p>Descripcion: " +descripcion +"</p>");
+        printWriter.print("</body>");
+        printWriter.print("</html>");
+        printWriter.close();
+    }
+}
---
+++ src/main/webapp/contactoGet.jsp
@@ -0,0 +1,33 @@
+<%-- 
+    Document   : contactoGet
+    Created on : Apr 15, 2023, 4:55:02 PM
+    Author     : JuaN
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Pagina de Registro de Contacto</h1>
+        <form action="<%= request.getContextPath()%>/ContactoServlet" method="GET">
+            <label>Nombre:</label>
+            <input type="text" name="nombre">
+            <br>
+            <label>Email:</label>
+            <input type="email" name="emailId">
+            <br>
+            <label>Telefono:</label>
+            <input type="text" name="telefono">
+            <br>
+            <label>Descripcion:</label>
+            <input type="text" name="descripcion">
+            <br>
+            <input type="submit" value="Registrar">
+        </form>
+            
+    </body>
+</html>
